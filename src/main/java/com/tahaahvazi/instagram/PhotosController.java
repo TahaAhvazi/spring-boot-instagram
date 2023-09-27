899f6852899a8ae07cac9080d6d867cf9ed8576f
Taha ahvazi <86960501+TahaAhvazi@users.noreply.github.com>
2023-09-28 03:01:36 +0330

    Added the function to delete the photos rom the API

--- src/main/java/com/tahaahvazi/instagram/PhotosController.java
+++ src/main/java/com/tahaahvazi/instagram/PhotosController.java
@@ -1,11 +1,13 @@
 package com.tahaahvazi.instagram;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,21 +17,30 @@ public class PhotosController {
 
     private Map<String, Photos> dataBase = new HashMap<>(){{
         put("1", new Photos("1","TahaAhvazi.png"));
+        put("2", new Photos("2","AlirezaAhvazi.png"));
+        put("3", new Photos("3","HosseinMoradi.png"));
+        put("4", new Photos("4","MahsaBeyranvand.png"));
+
     }};
-    private List<Photos> db = List.of(new Photos("1", "TahaAhvazi.jpg"));
     @GetMapping("/")
     public String sayHello(){
         return "Hello World!";
     }
 
     @GetMapping("/photos")
-    public List<Photos> AllPhotos(){
-        return db;
+    public Collection<Photos> AllPhotos(){
+        return dataBase.values();
     }
     @GetMapping("/photos/{id}")
     public Photos getPhoto(@PathVariable String id){
         Photos photos = dataBase.get(id);
         if (photos == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return photos;
+    }
+    @DeleteMapping("/photos/{id}")
+    public void deletePhoto(@PathVariable String id){
+        Photos photo = dataBase.get(id);
+        if (photo == null) throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
+        dataBase.remove(id);
     }
 }
